12b79b9197f705aae83696aee803a7565123e54e
Shekar <gv.shekar@gmail.com>
2016-06-02 18:38:02 -0400

    Finished parsing map into a list of links. TODO: Start writing into xml file

--- src/map_extract_template.py
+++ src/map_extract_template.py
@@ -6,7 +6,7 @@ from sets import Set
 from datetime import datetime
 import graph_tool.util as gtu
 import sys, argparse, logging, math
-import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as ET
 
 # Gather our code in a main() function
 def main(args, loglevel):
@@ -21,24 +21,18 @@ class MapParser(object):
     num_nodes = 0
     num_roads = 0
 
-    def run(self,filename):
-        self.p = OSMParser(ways_callback=self.find_highway_nodes)
-        self.p.parse(args.osm_filename)
-        print("Highway nodes found")
-        self.p =  OSMParser(coords_callback=self.count_nodes)
-        self.p.parse(args.osm_filename)
-        self.p = OSMParser(ways_callback=self.connect_nodes)
-        self.p.parse(args.osm_filename)
-        
+    def run(self,args):
+        self.extract_network(args.osm_filename)
         if args.draw:
             self.draw(args.draw)
         if args.output:
             self.tntp(args.output)
         if args.vissim:
             self.vissim(args.vissim)
+
     def extract_network(self,osm_filename):
-        ne = NetworkExtractor()
-        self.g = ne.extract(osm_filename)
+        self.ne = NetworkExtractor()
+        self.ne.extract(osm_filename)
         
     def vissim(self, filename):
         """Data required for VISSIM file. 
@@ -64,14 +58,28 @@ class MapParser(object):
                     print point.attrib['x'], point.attrib['y']
         """
         #First Parse the graph into links and connectors
-        vertices = gtu.find_vertex_range(self.g,"in",[2,1000])
+        #vertices = (gtu.find_vertex_range(self.g,"in",[2,1000]))
+        #ver2 =  gtu.find_vertex_range(self.g,"out",[2,1000])
         
-        vert_zero = (gtu.find_vertex_range(self.g,"in",[0,0]) +
-                    gtu.find_vertex_range(self.g,"out",[0,0]))
-        print len(vertices) + len(vert_zero)
-        for vertex in vertices+vert_zero:
+        #vert_zero = gtu.find_vertex_range(self.g,"in",[0,0]) 
+        #vert_zero2 = gtu.find_vertex_range(self.g,"out",[0,0])
+        #print len(vertices) , len(ver2), len(vert_zero), len(vert_zero2)
+        #for vertex in vertices+vert_zero:
+        #    print str(self.vertex_properties['pos'][vertex][1])+','+str(self.vertex_properties['pos'][vertex][0])
+        print len(self.ne.edge_endpts)
+        print len(self.ne.link_list)
+        
+        tree = ET.parse("reference_inpx.inpx")
+        root = tree.getroot()
+        
 
 
+    def draw(self,op_filename):
+        graph_draw(self.ne.g,vertex_size=self.ne.vertex_properties['size'], 
+                pos=self.ne.vertex_properties['pos'],
+                edge_pen_width=self.ne.edge_properties['thickness'],
+                edge_end_marker=self.ne.edge_properties['marker'],
+                fit_view=True,output_size=(800,800),output=args.draw)
 
 class NetworkExtractor(object):
     nodes = 0
@@ -80,6 +88,8 @@ class NetworkExtractor(object):
     g = Graph()
     osm_to_graph = {}
     highway_nodes = Set()
+    edge_endpts = []
+    link_list = []
     
     #Network vertex visual properties
     vprop_pos = g.new_vertex_property("vector<double>")
@@ -89,11 +99,24 @@ class NetworkExtractor(object):
     vprop_font_size = g.new_vertex_property("int")
     vprop_text_rotation = g.new_vertex_property("double")
     vprop_fill_color = g.new_vertex_property("vector<double>")
+    vertex_properties = {
+                        'pos':vprop_pos,
+                        'size':vprop_size,
+                        'shape':vprop_shape,
+                        'text':vprop_text,
+                        'font_size':vprop_font_size,
+                        'text_rotation':vprop_text_rotation,
+                        'fill_color':vprop_fill_color,
+                        }
     node_size = 2.0
     
     #Network edge visual properties
     eprop_marker = g.new_edge_property("double")
     eprop_thickness = g.new_edge_property("double")
+    edge_properties = {
+                        'marker':eprop_marker,
+                        'thickness':eprop_thickness
+                        }
 
     
     #List of road types excluded from parsing
@@ -108,16 +131,17 @@ class NetworkExtractor(object):
         print "Created graph nodes"
         self.p = OSMParser(ways_callback=self.connect_nodes)
         self.p.parse(osm_filename)
-        return self.g
+        print len(self.edge_endpts)
+        #return self.g,self.vertex_properties,self.edge_properties,self.edge_endpts
 
     def find_highway_nodes(self, ways):
-        """Find all nodes with the tag 'Highway'"""
+       """Find all nodes with the tag 'Highway'"""
        for osm_id, tags, refs in ways:
             if 'highway' in tags and tags['highway'] not in self.highways_excluded:
                 for r in refs:
                     self.highway_nodes.add(r)
 
-   def count_nodes(self,nodes):
+    def count_nodes(self,nodes):
         """Counts the total number of nodes in a particular way"""
         for osm_id, lat, lng in nodes:
             if osm_id in self.highway_nodes:
@@ -133,26 +157,46 @@ class NetworkExtractor(object):
         for osm_id,tags,refs in ways:
             if ('highway' in tags and tags['highway'] not in
                 self.highways_excluded):
+                link = []
                 self.hw += 1
                 for i in range(len(refs) -1):
                     if (refs[i] in self.osm_to_graph and refs[i+1] in
                         self.osm_to_graph):
                         v1 = self.g.vertex(self.osm_to_graph[refs[i]])
                         v2 = self.g.vertex(self.osm_to_graph[refs[i+1]])
+                        
                         if i == 0:
                             e = self.g.add_edge(v2,v1)
                             self.vprop_size[v1] = self.node_size
-
+                            self.edge_endpts.append(v1)
+                            link.append(v1)
                             if 'oneway' not in tags:
                                 self.eprop_marker[e] = 2
-
                         else:
                             e = self.g.add_edge(v1,v2)
-
+                        
+                        link.append(v2)
                         if ('oneway' in tags and tags['oneway'] == 'yes'):
                             self.eprop_thickness[e] = 0.25
                             if i == len(refs)-2:
-   
+                                self.eprop_marker[e] = 2
+                        else:
+			    self.eprop_thickness[e] = 0.75
+			    self.eprop_marker[e] = 0
+							
+                        if self.vprop_size[v1] != self.node_size:
+                            self.vprop_size[v1] = 0.0
+                        if i==len(refs)-2:
+                            self.vprop_size[v2] = self.node_size
+                            self.edge_endpts.append(v2)
+                        if self.vprop_size[v2] != self.node_size:
+                            self.vprop_size[v2] = 0.0
+                        self.vprop_fill_color[v1] = [0.640625, 0, 0, 0.9]
+                        self.vprop_fill_color[v2] = [0.640625, 0, 0, 0.9]
+                        self.vprop_shape[v1] = 0
+                        self.vprop_shape[v2] = 0
+                self.link_list.append(link)
+                
     def node_data_extract(self, nodes):
         """After finding all higway nodes, get their lat and lng values"""
         for osm_id, lat, lng in nodes:
@@ -162,42 +206,6 @@ class NetworkExtractor(object):
                 self.vprop_pos[self.g.vertex(self.nodes)] = [lat,lng]
                 self.nodes+=1
 
-    def add_edges(self, ways):
-        """Add edges to the graph-tool Graph"""
-
-        for osm_id, tags, refs in ways:
-            if 'highway' in tags and tags['highway'] not in self.highways_excluded:
-                self.num_roads += 1
-                for i in range(len(refs)-1):
-                    if refs[i] in self.osm_to_graph and refs[i+1] in self.osm_to_graph:
-                        v1 = self.g.vertex(self.osm_to_graph[refs[i]])
-                        v2 = self.g.vertex(self.osm_to_graph[refs[i+1]])
-                        #Make sure that the road direction is correct
-                        if i ==0:
-                            e = self.g.add_edge(v2,v1)
-                            self.vprop_size[v1] = self.node_size
-                            if 'oneway' not in tags:
-                                self.eprop_marker[e] = 2
-                        else:
-                            e = self.g.add_edge(v1,v2)
-                        
-                        if 'oneway' in tags and tags['oneway'] == 'yes':
-                            self.eprop_thickness[e] = 0.25
-                            if i==len(refs)-2:
-                               self.eprop_marker[e] = 2
-                        else:
-                            self.eprop_thickness[e] = 0.75
-                            self.eprop_marker[e] = 0
-                        if self.vprop_size[v1] != self.node_size:
-                            self.vprop_size[v1] = 0.0
-                        if i==len(refs)-2:
-                            self.vprop_size[v2] = self.node_size
-                        if self.vprop_size[v2] != self.node_size:
-                            self.vprop_size[v2] = 0.0
-                        self.vprop_fill_color[v1] = [0.640625, 0, 0, 0.9]
-                        self.vprop_fill_color[v2] = [0.640625, 0, 0, 0.9]
-                        self.vprop_shape[v1] = 0
-                        self.vprop_shape[v2] = 0
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='OSM map parser to output network in different formats')
